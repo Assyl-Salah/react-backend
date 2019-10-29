ec2da9ca5a4d332eeedb2fbf7fa402d7dd11cef8
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-10-29 22:19:01 +0100

    LAB3TASK3_withoutpropererrorhandling

--- src/main/java/pw/react/backend/reactbackend/controller/Controller.java
+++ src/main/java/pw/react/backend/reactbackend/controller/Controller.java
@@ -1,13 +1,16 @@
 package pw.react.backend.reactbackend.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import pw.react.backend.reactbackend.entity.User;
+import pw.react.backend.reactbackend.error.ResourceNotFoundException;
 import pw.react.backend.reactbackend.respository.UserRepository;
 import pw.react.backend.reactbackend.service.UserService;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/controller")
@@ -28,8 +31,7 @@ public class Controller {
     @PostMapping("/create_new_user")
     public String CreatenewUser(@RequestBody User Newuser) {
 
-        if (userv.check(Newuser.getLogin()) == null)
-        {
+        if (userv.check(Newuser.getLogin()) == null) {
             urepos.save(new User(Newuser.getLogin(), Newuser.getFirst_name(),
                     Newuser.getLast_name(), Newuser.getDate_of_birth(), Newuser.getActive()));
             return "new user is created";
@@ -43,5 +45,32 @@ public class Controller {
         if (user == null)
             return "No user was found";
         return user.toString();
+    }
+
+    @GetMapping(value = "/findById/{id}")
+    public String retrieveid(@PathVariable long id) {
+        Optional<User> user = urepos.findById(id);
+        if (!(user).isPresent())
+            return "No user was found";
+        return user.toString();
+    }
+
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateStudent(@RequestBody User user, @PathVariable long id) {
+        Optional<User> users = urepos.findById(id);
+        if (!((Optional) users).isPresent())
+            return ResponseEntity.notFound().build();
+
+        user.setId(id);
+        urepos.save(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable long id)
+    {
+        urepos.deleteById(id);
+        throw new ResourceNotFoundException("user", "id", id);
     }
 }
